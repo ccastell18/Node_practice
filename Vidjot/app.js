a03949abc96539773fa6edbf1feb9b763668de14
ccastell18 <c.castellano18@gmail.com>
2019-03-02 16:54:58 -0600

    idea validation

--- Vidjot/app.js
+++ Vidjot/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -22,6 +23,9 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // Index Route
 app.get('/', (req, res) => {
@@ -40,6 +44,34 @@ app.get('/about', (req, res) => {
 app.get('/ideas/add', (req, res) =>{
   res.render('ideas/add');
 })
+
+//process form
+app.post('/ideas', (req, res) =>{
+  let errors = [];
+  if(!req.body.title){
+    errors.push({text: 'Please add a title'})
+  }
+  if(!req.body.details){
+    errors.push({text: 'Please add a details'})
+  }
+
+  if(errors.length > 0){
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    })
+  }else{
+    res.send('passed');
+  }
+})
+
+
+
+
+
+
+
 
 const port = 5000;
 

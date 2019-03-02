f724e2c2cbd18701c07a57d0055ae4d7d0cfd79c
ccastell18 <c.castellano18@gmail.com>
2019-03-01 22:22:23 -0600

    ignore

--- Vidjot/app.js
+++ Vidjot/app.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+//index route
+app.get('/', (req,res) =>{
+    res.send("Index");
+});
+
+app.get('/about', (req, res) =>{
+    res.send('About');
+})
+
 const port = 5000;
 
 app.listen(port, () =>{

7a5844494d1ab932386c907853449941cddb1623
ccastell18 <c.castellano18@gmail.com>
2019-03-01 23:29:15 -0600

    views

--- Vidjot/app.js
+++ Vidjot/app.js
@@ -1,23 +1,31 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
 //how middleware works
-app.use(function(req,res,next){
-    // console.log(Date.now());
-    req.name="Chris";
-    next();
-})
+// app.use(function(req,res,next){
+//     // console.log(Date.now());
+//     req.name="Chris";
+//     next();
+// })
 
+//handlebars middleware
+app.engine('handlebars',exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 //Index route
 app.get('/',(req,res) =>{
-    console.log(req.name)
-    res.send("index");
+    const title = 'Hello World';
+    res.render("index",{
+        title: title
+    });
 })
 
 //About
 app.get('/about', (req, res) =>{
-    res.send('About');
+    res.render('About');
 })
 
 const port = 5000;

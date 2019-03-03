bf219596aeb6a6b9eb3997e1266b794e8a8a58ee
ccastell18 <c.castellano18@gmail.com>
2019-03-02 22:49:16 -0600

    user registration

--- Vidjot/routes/users.js
+++ Vidjot/routes/users.js
@@ -12,4 +12,28 @@ router.get('/login', (req, res) =>{
     res.render('users/register')
   })
 
+  //register form POST
+  router.post('/register', (req, res) => {
+    let errors = [];
+    if(req.body.password != req.body.password2){
+      errors.push({text: 'Passwords do not match' })
+    }
+
+    if(req.body.password.length < 4){
+      errors.push({text: 'Password musht be 4 characters'});
+    }
+
+    if(errors.length > 0){
+      res.render('users/register', {
+        errors: errors,
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        password2: req.body.password2
+      })
+    }else{
+      res.send("passed");
+    }
+  })
+
 module.exports = router;

21b7478a75ec871c6209b3b37253076efbc8355c
ccastell18 <c.castellano18@gmail.com>
2019-03-03 22:19:29 -0600

    logout

--- Vidjot/routes/users.js
+++ Vidjot/routes/users.js
@@ -83,4 +83,10 @@ router.post('/login', (req,res, next) =>{
     }
   })
 
+  //logout user
+router.get('/logout', (req, res) =>{
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/users/login');
+})
 module.exports = router;
